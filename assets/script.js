588833ed233642b4ac3d0f39a906541a2417662a
d4nielantoni <danielantoni7575@gmail.com>
2024-05-31 17:27:00 -0300

    adicionando loading em javascript

--- assets/script.js
+++ assets/script.js
@@ -1,29 +1,39 @@
-document.getElementById('imcForm').addEventListener('submit', function(event) {
-    event.preventDefault();
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('imcForm').addEventListener('submit', function(event) {
+        event.preventDefault();
 
-    let gender = document.getElementById('gender').value;
-    let height = parseFloat(document.getElementById('height').value) / 100;
-    let weight = parseFloat(document.getElementById('weight').value);
+        let gender = document.getElementById('gender').value;
+        let height = parseFloat(document.getElementById('height').value) / 100;
+        let weight = parseFloat(document.getElementById('weight').value);
 
-    if (isNaN(height) || isNaN(weight) || height <= 0 || weight <= 0) {
-        alert('Por favor, insira valores válidos.');
-        return;
-    }
+        if (isNaN(height) || isNaN(weight) || height <= 0 || weight <= 0) {
+            alert('Por favor, insira valores válidos.');
+            return;
+        }
 
-    let imc = weight / (height * height);
-    let classification = '';
+        let loader = document.getElementById('loader');
+        
+        loader.style.display = 'block';
 
-    if (imc < 18.5) {
-        classification = 'Abaixo do peso';
-    } else if (imc < 24.9) {
-        classification = 'Peso normal';
-    } else if (imc < 29.9) {
-        classification = 'Sobrepeso';
-    } else {
-        classification = 'Obesidade';
-    }
+        setTimeout(function() {
+            let imc = weight / (height * height);
+            let classification = '';
 
-    let resultText = `Seu IMC é ${imc.toFixed(2)} (${classification}).`;
+            if (imc < 18.5) {
+                classification = 'Abaixo do peso';
+            } else if (imc < 24.9) {
+                classification = 'Peso normal';
+            } else if (imc < 29.9) {
+                classification = 'Sobrepeso';
+            } else {
+                classification = 'Obesidade';
+            }
 
-    document.getElementById('result').textContent = resultText;
+            let resultText = `Seu IMC é ${imc.toFixed(2)} (${classification}).`;
+
+            document.getElementById('result').textContent = resultText;
+            
+            loader.style.display = 'none';
+        }, 400);
+    });
 });
